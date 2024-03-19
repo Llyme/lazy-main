5f4b42022ab3c6233d16cfc15a2436ead479ff12
Michael Edmund Wong <infinitex777@gmail.com>
2024-03-19 09:52:13 +0800

    Now catches error from main function & added error handler.

--- lazyMain.js
+++ lazyMain.js
@@ -13,6 +13,7 @@ export class LazyMain {
      * 
      * @param {object} kwargs
      * @param {function(...args): Promise<boolean>} kwargs.main
+     * @param {function(Error): void|undefined} kwargs.errorHandler
      * @param {number|undefined} kwargs.sleepMin
      * @param {number|undefined} kwargs.sleepMax
      * @param {number|undefined} kwargs.loopCount
@@ -20,12 +21,14 @@ export class LazyMain {
     constructor(kwargs) {
         const {
             main,
+            errorHandler,
             sleepMin,
             sleepMax,
             loopCount
         } = kwargs;
 
         this.main = main;
+        this.errorHandler = errorHandler;
         this.sleepMinMS = sleepMin ?? this.sleepMinMS;
         this.sleepMaxMS = sleepMax ?? this.sleepMaxMS;
         this.loopCount = loopCount ?? this.loopCount;
@@ -38,8 +41,19 @@ export class LazyMain {
 
     async run(...args) {
         while (true) {
+            let ok = false;
+
             const t1 = performance.now();
-            const ok = await this.main(...args);
+
+            try {
+                ok = await this.main(...args);
+            } catch (e) {
+                console.log('An error ocurred.', e);
+
+                if (this.errorHandler != null)
+                    this.errorHandler(e);
+            }
+
             const sleepTime = this.#sleepTime;
 
             if (this.loopCount > 0)
