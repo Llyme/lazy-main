a726fdc239f82dc7c5dd59ec828715ae6bccd1ad
Michael Edmund Wong <infinitex777@gmail.com>
2024-03-27 09:43:36 +0800

    Fixed naming convention.

--- lazy_main/lazy_main.py
+++ lazy_main/lazy_main.py
@@ -8,21 +8,34 @@ class LazyMain:
     def __init__(
         self,
         main: Callable[..., bool],
-        errorHandler: Callable[[Exception], None] = None,  # type: ignore
-        sleepMin=3,
-        sleepMax=5,
-        loopCount=-1,
+        error_handler: Callable[[Exception], None] = None,  # type: ignore
+        print_logs: bool = True,
+        sleep_min=3,
+        sleep_max=5,
+        loop_count=-1,
     ):
+        """
+        main: The function that will be called every loop.
+        error_handler: If the `main` function throws an error, this will be called.
+        print_logs: If it should print logs.
+        sleep_min: Minimum sleep time, in seconds.
+        sleep_max: Maximum sleep time, in seconds.
+        loop_count: How many times this will loop. If -1, it will infinitely loop.
+        """
         self.main = main
-        self.errorHandler = errorHandler
-        self.sleepMin = sleepMin
-        self.sleepMax = sleepMax
-        self.loopCount = loopCount
+        self.error_handler = error_handler
+        self.print_logs = print_logs
+        self.sleep_min = sleep_min
+        self.sleep_max = sleep_max
+        self.loop_count = loop_count
 
-    def __getSleepTime(self):
-        return random() * self.sleepMin + self.sleepMax - self.sleepMin
+    def __get_sleep_time(self):
+        return random() * self.sleep_min + self.sleep_max - self.sleep_min
 
     def run(self, *args, **kwargs):
+        """
+        Starts the loop.
+        """
         while True:
             ok = False
             t1 = perf_counter()
@@ -30,26 +43,28 @@ class LazyMain:
             try:
                 ok = self.main(*args, **kwargs)
             except Exception as e:
-                print("An error ocurred.", e)
+                if self.print_logs:
+                    print("An error ocurred.", e)
                 traceback.print_exc()
 
-                if self.errorHandler != None:
-                    self.errorHandler(e)
+                if self.error_handler != None:
+                    self.error_handler(e)
 
-            sleepTime = self.__getSleepTime()
+            sleep_time = self.__get_sleep_time()
 
-            if self.loopCount > 0:
-                self.loopCount -= 1
+            if self.loop_count > 0:
+                self.loop_count -= 1
 
             if ok:
                 t2 = perf_counter()
 
-                print(f"Done in {t2 - t1:.2f}s.")
+                if self.print_logs:
+                    print(f"Done in {t2 - t1:.2f}s.")
 
-                if self.loopCount > 0:
-                    print(f"Sleeping for {sleepTime:.2f}s...")
+                if self.loop_count > 0 and self.print_logs:
+                    print(f"Sleeping for {sleep_time:.2f}s...")
 
-            if self.loopCount == 0:
+            if self.loop_count == 0:
                 break
 
-            sleep(sleepTime)
+            sleep(sleep_time)
